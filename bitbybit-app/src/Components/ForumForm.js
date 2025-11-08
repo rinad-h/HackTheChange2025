47d19843439ec767231fbc8423bd03544cce2205
daadhaymour <dhay5874@gmail.com>
2025-11-08 13:37:40 -0700

    added components for forum

---
+++ bitbybit-app/src/Components/ForumForm.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const ForumForm = () => {
+    return(
+        <div>
+            <div className='Location'>
+                <input placeholder='Add Location'/>
+            </div>
+            <div className='Forum-Options'></div>
+            <div className='Description'></div>
+            
+        </div>
+    )
+}
