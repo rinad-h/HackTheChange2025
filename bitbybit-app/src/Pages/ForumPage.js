8b06bc1aa6cf83661dd471dd23fb0072375af30b
rinad-h <rinad.hamid@ucalgary.ca>
2025-11-08 20:27:07 -0700

    Create ForumPage.js

---
+++ bitbybit-app/src/Pages/ForumPage.js
@@ -0,0 +1,36 @@
+import React, {useState, useEffect} from 'react';
+import ForumForm from "../Components/ForumForm";
+import ForumPost from "../Components/ForumPost";
+//import Comment from "../Components/Comment";
+
+import API from "../apiController";
+
+const ForumPage = () => {
+    const [showPopup, setShowPopup] = useState(false);
+    const [posts, setPosts] = useState([]);
+
+    const togglePopup = () =>{
+        setShowPopup(!showPopup);
+    }
+
+    const loadPosts = async () => {
+        const rows = await API.listPosts();
+        setPosts(rows);
+    };
+
+    useEffect(() => {loadPosts();}, []);
+    return(
+        <div>
+            <div className='Forum-Comments'>
+
+            </div>
+            <button onClick={togglePopup}>Add New Forum</button>
+           {showPopup &&  <ForumForm onCreated={() => {loadPosts(); setShowPopup(false); }} /> }
+
+            {posts.map(p => <ForumPost key={p.id} post={p} />)}
+          
+        </div>
+    )
+}
+
+export default ForumPage;
