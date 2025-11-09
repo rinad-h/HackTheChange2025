880926f2de97dc4f7b798694632d65193098daed
rinad-h <rinad.hamid@ucalgary.ca>
2025-11-09 11:32:03 -0700

    deploying

--- bitbybit-app/backend/server.js
+++ bitbybit-app/backend/server.js
@@ -3,9 +3,11 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const bcrypt = require("bcrypt");
 const sqlite3 = require("sqlite3").verbose();
+const path = require("path");
+
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -163,4 +165,12 @@ app.post("/posts/:id/comments", (req, res) => {
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
+});
+
+// Serve React static files
+app.use(express.static(path.join(__dirname, "../build"))); // ../build points to the build folder
+
+// Catch-all for React routing
+app.get(/.*/, (req, res) => {
+  res.sendFile(path.join(__dirname, "../build", "index.html"));
 });
